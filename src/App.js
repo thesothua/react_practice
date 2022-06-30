41914056acb58968f768a7f67789b91bf1791ec9
praveen <praveensuthar9553@gmail.com>
2022-06-30 22:00:33 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -5,11 +5,23 @@ import About from './components/About';
 import Navbar from './components/Navbar'
 import TextForm from './components/TextForm';
 import { useState } from 'react';
+import Alert from './components/Alert';
 
 
 function App() {
 
 const [mode, setmode] = useState('light') //dark mode enable or not
+
+ const [alert, setalert] = useState(null);
+
+const showAlert = (message, type)=> {
+  setalert({
+    msg: message,
+    type: type
+  })
+
+  // console.log(setalert);
+}
 
 const togglemode = () =>{
 
@@ -18,24 +30,25 @@ const togglemode = () =>{
     setmode('dark');
     document.body.style.backgroundColor = 'black';
     document.body.style.color = 'white';
+
+    showAlert('You are using dark mode', 'primary');
   }else{
     setmode('light');
     document.body.style.backgroundColor = 'white';
+    showAlert("You are using light mode", 'success');
   }
 
 }
 
-// console.log(mode); 
+// console.log(alert); 
 
   return (
    <>
    <Navbar logo={'FIRST APP'} about='about us' mode={mode} toggleMode={togglemode}/>
+   <Alert text={alert}/>
    <TextForm/>
    <About/>
    </>
-
-
- 
   );
 }
 
